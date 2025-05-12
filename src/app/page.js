b029fd0aa492ae0dd1833b637b14755d0bc25317
Jesper <jefy0001@stud.kea.dk>
2025-05-12 13:48:27 +0200

    Remove Kuratoredit component from Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 "use client";
 import Footer from "./components/Footer";
-import Kuratoredit from "./Kuratoredit";
 
 export default function Home() {
   return (
     <section>
-      <Kuratoredit variant="close" />
       <Footer />
     </section>
   );
