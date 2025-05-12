8c9043936254b0b5f59d26f02248b4d6db3b3956
Oliver Valter <olva0001@kea.stud.dk>
2025-05-12 13:28:38 +0200

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Footer from "./components/Footer";
-import Singleview from "./eventliste/Singleview";
+import Singleview from "./singleview/Singleview";
 
 export default function Home() {
   return (
