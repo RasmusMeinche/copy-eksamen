cbe8ba0af340c07d3169f2683bb2fcd8fdb31593
Jesper <jefy0001@stud.kea.dk>
2025-05-12 13:48:00 +0200

    Alle knapper lavet færdig, med props til brug over hele løsningen

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 "use client";
 import Footer from "./components/Footer";
+import Kuratoredit from "./Kuratoredit";
 
 export default function Home() {
-  return <Footer />;
+  return (
+    <section>
+      <Kuratoredit variant="close" />
+      <Footer />
+    </section>
+  );
 }
