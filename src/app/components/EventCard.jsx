c71572fb6d8c478eb050658991e9785096a3b5c8
Rasmus <rame0001@stud.kea.dk>
2025-05-13 00:41:05 +0200

    refactor EventCard component for improved layout and styling

--- src/app/components/EventCard.jsx
+++ src/app/components/EventCard.jsx
@@ -1,19 +1,33 @@
 import { getLocalData } from "@/lib/local";
+import Image from "next/image";
 
 export const EventCard = async () => {
   const localData = await getLocalData();
   console.log(localData);
 
   return (
-    <section className="flex flex-col justify-center items-center py-8 bg-[#800000]">
+    <section className="grid grid-cols-[auto,1fr,auto] justify-center items-center py-8 bg-[#800000]">
           {localData.map((event) => (
-            <div className="flex flex-col inset-shadow-sm bg-white p-4 w-[80%]" key={event.id}>
-              <h1>{event.title}</h1>
-              <p>{event.description}</p>
-              <p>{event.date}</p>
-              <p>{event.curator}</p>
-              <p>{event.totalTickets}</p>
-              <p>{event.bookedTickets}</p>
+            <div className="col-start-2 col-end-4 grid grid-cols-[auto,1fr] inset-shadow-sm border mb-4 mt-4 border-white text-white w-full" key={event.id}>
+                <div className="flex gap-4 relative">
+                <div className="col-1 flex">
+                    <Image
+                        src={event.image}
+                        alt={event.title}
+                        width={300}
+                        height={300}
+                        className="bg-amber-50"
+                    />
+                </div>
+                <div>
+                    <h1>{event.title}</h1>
+                    <p>{event.description}</p>
+                    <p>{event.date}</p>
+                    <p>{event.curator}</p>
+                    <p>{event.totalTickets}</p>
+                    <p>{event.bookedTickets}</p>
+              </div>
+              </div>
             </div>
           ))}
     </section>
