625f9af6d42dbd23fb59a82e45998b20128cf7b1
RasmusMeinche <rame0001@stud.kea.dk>
2025-05-14 13:37:26 +0200

    feat: update dependencies and enhance EventCard component
    - Added @tailwindcss/vite to dependencies and updated tailwindcss version in package.json.
    - Refactored EventCard component to dynamically fetch and display event images and details.
    - Removed unnecessary client directive from Eventliste component.
    - Updated layout.js to use Roboto Condensed font and adjusted HTML structure.
    - Created tailwind.config.js to configure Tailwind CSS with custom font settings.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';import { Roboto, Lora } from "next/font/google";
 
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto-condensed",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,13 +27,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
+       <html lang="da" className={robotoCondensed.variable}>
+      <body className="font-roboto-condensed">{children}</body>
+    </html>
     </ClerkProvider>
   );
 }
