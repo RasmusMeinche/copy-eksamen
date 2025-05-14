45065abcfda5c7b7a3662bb386c6669219d96cf4
RasmusMeinche <rame0001@stud.kea.dk>
2025-05-14 15:22:04 +0200

    fix: remove unnecessary blank line in EventCard component

--- src/app/components/EventCard.jsx
+++ src/app/components/EventCard.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Roboto } from "next/font/google";
 import Button from "./Button";
 
+
 export const EventCard = async () => {
   const localData = await getLocalData();
   console.log(localData);
