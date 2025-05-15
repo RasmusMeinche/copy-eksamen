1b80ac1571b786a5208df49d2cba5ed0bd6b14d9
Rasmus <rame0001@stud.kea.dk>
2025-05-15 13:50:36 +0200

    fix: correct button hover border style in Kuratoredit component

--- src/app/components/Kuratoredit.jsx
+++ src/app/components/Kuratoredit.jsx
@@ -3,7 +3,7 @@ import { IoMdClose } from "react-icons/io";
 
 const Kuratoredit = ({ variant = "edit" }) => {
   return (
-    <button className="group flex items-center gap-2 px-4 py-2 border bg-white cursor-pointer text-[#800000] hover:border border-white hover:bg-[#800000] hover:text-white transition">
+    <button className="group flex items-center gap-2 px-4 py-2 border bg-white cursor-pointer text-[#800000] hover:border-2 border-white hover:bg-[#800000] hover:text-white transition">
       {variant === "edit" ? (
         <GoPencil className="text-lg text-[#800000] group-hover:text-white transition" />
       ) : (
