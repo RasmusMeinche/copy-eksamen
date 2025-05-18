7deef9f70938ae16e988c234a11ceedb9c878c9b
RasmusMeinche <rame0001@stud.kea.dk>
2025-05-19 00:39:38 +0200

    fix: update SimpleDialog to fetch local data and improve layout

--- src/app/components/PopOver.jsx
+++ src/app/components/PopOver.jsx
@@ -1,9 +1,10 @@
-'use client';
-
 import * as Dialog from '@radix-ui/react-dialog';
 import Kuratoredit from './Kuratoredit';
+import { getLocalData } from '@/lib/local';
 
-export default function SimpleDialog() {
+export default async function SimpleDialog() {
+  const data = await getLocalData();
+
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
@@ -13,14 +14,17 @@ export default function SimpleDialog() {
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50 z-50" />
         <Dialog.Content
-          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-6 rounded-lg shadow-lg z-50 max-w-md w-full"
+          className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-6 rounded-lg shadow-lg z-50 max-w-7xl w-full"
         >
-          <Dialog.Title className="text-lg font-semibold mb-4">Hej!</Dialog.Title>
-          <p>Dette er en dialogboks midt på skærmen.</p>
-          
+          <Dialog.Title className="text-lg font-semibold mb-4 flex justify-center">Rediger Event</Dialog.Title>
+
+          {data.map((item, index) => (
+            <p key={index}>{item.title}</p>
+          ))}
+
           <Dialog.Close asChild>
-            <button className="mt-4 px-4 py-2 bg-[#800000] text-white rounded hover:bg-[#a00000] transition">
-              Luk
+            <button className=" fixed top-[5px] right-[20px] mt-4 px-4 py-2 bg-[#800000] text-white rounded hover:bg-[#a00000] transition">
+              X
             </button>
           </Dialog.Close>
         </Dialog.Content>
