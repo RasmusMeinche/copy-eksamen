8f9ffe8e2233fb8c0ba04eea4563c1e6c16877d9
Rasmus <rame0001@stud.kea.dk>
2025-05-26 13:02:38 +0200

    feat: add artwork selection and load more functionality in CreateEvent component

--- src/app/components/CreateEvent.jsx
+++ src/app/components/CreateEvent.jsx
@@ -115,6 +115,40 @@ export default function CreateEvent({ onCancel }) {
           onChange={handleDescriptionChange}
         />
       </div>
+        <h2 className="font-bold text-left pl-4 text-xl my-4">Vælg kunstværker til event:</h2>
+        <ul className="grid grid-cols-5 gap-4 my-6 mx-2">
+          {artworks
+            .filter((art) => art.has_image)
+            .map((art) => {
+              const imgSrc =
+                art.image_thumbnail ||
+                art.image?.thumbnail ||
+                art.image?.web ||
+                art.images?.[0]?.web;
+              return (
+                <li
+                  className="shadow-xl/20 rounded-md p-4 bg-white hover:bg-gray-200 ease-in duration-100"
+                  key={art.id}
+                >
+                  {imgSrc ? (
+                    <img
+                      src={imgSrc}
+                      alt={art.titles?.[0]?.title || "Artwork"}
+                      title={`ID: ${art.id}`}
+                      className="mt-2 w-full h-auto object-cover"
+                    />
+                  ) : (
+                    <div className="mt-2 text-gray-500">No image available</div>
+                  )}
+                </li>
+              );
+            })}
+        </ul>
+        <button
+          type="button"
+          onClick={loadMore}
+          className="text-[#800000] border-2 border-[#800000] py-2 px-4 mb-10 mt-5 mx-auto hover:bg-[#800000] hover:text-white">Indlæs flere
+        </button>
 
       <div className="flex justify-center p-4 gap-20">
         <button
