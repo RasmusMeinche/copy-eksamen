132e895ea1a0f9cdf0cea41eb0796b81a80c7635
Oliver Valter <olva0001@kea.stud.dk>
2025-05-26 15:37:16 +0200

    added button design

--- src/app/components/PDFTicketButton.jsx
+++ src/app/components/PDFTicketButton.jsx
@@ -5,9 +5,8 @@ const PDFTicketButton = ({ event, tickets }) => (
   <PDFDownloadLink
     document={<TicketDocument event={event} tickets={tickets} />}
     fileName={`billet-${event.title}.pdf`}
-    className="bg-white text-black px-4 py-2 rounded"
-  >
-    Download Billetter
+    className="relative border border-white text-white px-4 py-5 w-40 h-14 hover:bg-white hover:text-[#800000]">
+    <span className="absolute bottom-2 left-2 text-left">Download Billetter</span>
   </PDFDownloadLink>
 );
 export default PDFTicketButton;
