5333bb31d49e3d2942333dee2b37a4c0a64b9bbf
Jesper <jefy0001@stud.kea.dk>
2025-06-01 23:43:27 +0200

    Refactor middleware to simplify public route handling and remove unnecessary route matcher

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,22 @@
 // src/middleware.ts
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Liste over offentlige ruter
-// Her kan du tilføje flere ruter, som ikke skal beskyttes
-const isPublicRoute = createRouteMatcher([
-  "/", // forside
-  "/events(.*)", // event routes
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/login(.*)",
-  "/checkout(.*)",
-  "/booking-bekraeftelse(.*)",
-  "/singleview(.*)",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
+export default clerkMiddleware({
+  publicRoutes: [
+    "/",
+    "/events(.*)",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/login(.*)",
+    "/checkout(.*)",
+    "/booking-bekraeftelse(.*)",
+    "/singleview(.*)",
+  ],
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
